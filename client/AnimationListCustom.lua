b422e23b7a060ba37e84db3032d651261387a5f1
GeekGarage <rasmus@geekgarage.dk>
2022-11-30 21:34:39 +0100

    fix emote

--- client/AnimationListCustom.lua
+++ client/AnimationListCustom.lua
@@ -5,7 +5,40 @@ CustomDP.Expressions = {}
 CustomDP.Walks = {}
 CustomDP.Shared = {}
 CustomDP.Dances = {}
-CustomDP.AnimalEmotes = {}
+CustomDP.AnimalEmotes = {
+    ["bdogindicateahead"] = {
+        "creatures@rottweiler@indication@",
+        "indicate_ahead",
+        "Indicate Ahead (big dog)",
+        AnimationOptions = {
+            EmoteLoop = true
+        }
+    },
+    ["bdogindicatehigh"] = {
+        "creatures@rottweiler@indication@",
+        "indicate_high",
+        "Indicate High (big dog)",
+        AnimationOptions = {
+            EmoteLoop = true
+        }
+    },
+    ["bdogindicatelow"] = {
+        "creatures@rottweiler@indication@",
+        "indicate_low",
+        "Indicate Low (big dog)",
+        AnimationOptions = {
+            EmoteLoop = true
+        }
+    },    
+    ["bdogshiftpaws"] = {
+        "creatures@rottweiler@tricks@",
+        "paw_right_loop_left",
+        "paw right left loop (big dog)",
+        AnimationOptions = {
+            EmoteLoop = true
+        }
+    }
+}
 CustomDP.Emotes = {}
 CustomDP.PropEmotes = {}
 
