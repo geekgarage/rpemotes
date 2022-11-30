a507e5536404f2886727c9f605b2e0cd931da63d
GeekGarage <rasmus@geekgarage.dk>
2022-11-30 23:00:02 +0100

    update custom emote array to empty

--- client/AnimationListCustom.lua
+++ client/AnimationListCustom.lua
@@ -5,32 +5,7 @@ CustomDP.Expressions = {}
 CustomDP.Walks = {}
 CustomDP.Shared = {}
 CustomDP.Dances = {}
-CustomDP.AnimalEmotes = {    
-    ["bdogtest"] = {
-        "creatures@rottweiler@move",
-        "dump_loop",
-        "TEST Dump (big dog)",
-        AnimationOptions = {
-            EmoteLoop = true,
-            EmoteMoving = false,
-            PtfxAsset = "scr_amb_chop",
-            PtfxName = "ent_anim_dog_poo",
-            PtfxNoProp = false,
-            PtfxPlacement = {
-                0.10,
-                -0.08,
-                0.0,
-                0.0,
-                90.0,
-                180.0,
-                1.0
-            },
-            PtfxInfo = Config.Languages[Config.MenuLanguage]['poop'],
-            PtfxWait = 3000,
-            PtfxCanHold = true
-        }
-    },
-}
+CustomDP.AnimalEmotes = {}
 CustomDP.Emotes = {}
 CustomDP.PropEmotes = {}
 
