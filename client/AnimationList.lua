3eb78db744f65f34a310ae8babc27b5f70a5bb2d
GeekGarage <rasmus@geekgarage.dk>
2022-12-02 06:43:35 +0100

    update emote

--- client/AnimationList.lua
+++ client/AnimationList.lua
@@ -564,7 +564,7 @@ RP.Shared = {
         AnimationOptions = {
             EmoteMoving = false,
             EmoteLoop = true,
-            SyncOffsetFront = -0.85,
+            SyncOffsetFront = -0.35,
             SyncOffsetHeading = 0
         },
         AdultAnimation = true
