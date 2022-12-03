33a25cebee8daa1e4807adfe74ac967b41067fcb
GeekGarage <rasmus@geekgarage.dk>
2022-12-03 17:41:27 +0100

    update debug print

--- client/EmoteMenu.lua
+++ client/EmoteMenu.lua
@@ -165,7 +165,7 @@ function AddEmoteMenu(menu)
             -- check if current value b is default
             -- if not "default" and value b matches current player model
             -- then x, y, z, otheremotename = table.unpack(b)
-            print("EMOTE: " .. a)
+            print("EMOTE: " .. a .. " : " .. b)
             x, y, z, otheremotename = table.unpack(b)
             if x ~= nil then
                 print(x)
