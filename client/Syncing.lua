5155044b532f054ecbbd2447e0e97194a0572954
GeekGarage <rasmus@geekgarage.dk>
2022-12-04 18:14:41 +0100

    fix debug print

--- client/Syncing.lua
+++ client/Syncing.lua
@@ -36,6 +36,8 @@ RegisterNetEvent("SyncPlayEmote")
 AddEventHandler("SyncPlayEmote", function(emote, player)
     print("SyncPlayEmote - 'emote':")
     print(emote)
+    print("RP.Shared[" .. emote .. "]")
+    print(type(RP.Shared[emote]))
     EmoteCancel()
     Wait(300)
     targetPlayerId = player
