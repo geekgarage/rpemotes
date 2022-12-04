437163add8c9823ad2652fc695548abe43774230
GeekGarage <rasmus@geekgarage.dk>
2022-12-04 01:24:28 +0100

    Debug update

--- client/EmoteMenu.lua
+++ client/EmoteMenu.lua
@@ -164,7 +164,7 @@ function AddEmoteMenu(menu)
             --print(b.default)
             -- START - Check for specific model AnimationOptions
             if b.default ~= nil then
-                print("we got arrays", type(b[1]))
+                print("we got arrays", b[1])
                 --for c, d in pairs(b) do
                 --    if c ~= "default" and GetHashKey(c) == GetEntityModel(PlayerPedId()) then
                 --        b = d
