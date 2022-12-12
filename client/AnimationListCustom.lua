03b140f4c962516f329d75ee0138f75eb045ea76
GeekGarage <info@geekgarage.dk>
2022-12-12 18:34:21 +0100

    update Text

--- client/AnimationListCustom.lua
+++ client/AnimationListCustom.lua
@@ -1,5 +1,7 @@
 -- Emotes you add in the file will automatically be added to AnimationList.lua
 -- If you have multiple custom list files they MUST be added between AnimationList.lua and Emote.lua in fxmanifest.lua!
+-- Don't change 'CustomDP' it is local to this file!
+
 local CustomDP = {}
 
 CustomDP.Expressions = {}
